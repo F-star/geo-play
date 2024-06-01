bc5678a3c6b75602e181ef88cc3e2e7fba43619f
xigua <hhbuhuiai@163.com>
2024-06-01 23:29:22 +0800

    update

--- src/page/draw-arc/geo.ts
+++ src/page/draw-arc/geo.ts
@@ -1,4 +1,4 @@
-import { distance, getSweepAngle } from '../../geo';
+import { distance } from '../../geo';
 import { Point } from '../../type';
 
 export const getArcPoint = (center: Point, radius: number, angle: number) => {
@@ -21,6 +21,15 @@ export const getArc2Center = (
   largeArc: boolean,
 ) => {
   const dist = distance(start, end);
+  // 半径太小，无法构成圆。
+  // 做特殊处理，radius 替换为 start 到 end 的距离除以 2
+  // 此时圆心就是两点的中点
+  if (radius * 2 < dist) {
+    return {
+      x: start.x / 2 + end.x / 2,
+      y: start.y / 2 + end.y / 2,
+    };
+  }
   const cos = Math.min(dist / 2 / radius, 1);
   const dAngle = Math.acos(cos);
   const startToEndAngle = Math.atan((end.y - start.y) / (end.x - start.x));
--- src/page/draw-arc/index.ts
+++ src/page/draw-arc/index.ts
@@ -1,4 +1,6 @@
 import { drawNumText, fillPoints } from '../../draw-util';
+import { distance } from '../../geo';
+import { Point } from '../../type';
 import { getArc2Center, getArcPoint } from './geo';
 
 const canvas = document.querySelector('canvas')!;
@@ -25,19 +27,18 @@ const drawArc1 = (ctx: CanvasRenderingContext2D) => {
 drawArc1(ctx);
 
 /*********** 方式 2 **********/
-const drawArc2 = () => {
-  const start = { x: 100, y: 100 };
-  const end = { x: 250, y: 200 };
-  const radius = 100;
-  const sweep = true;
-  const largeArc = false;
-
+const drawArc2 = (
+  start: Point,
+  end: Point,
+  radius: number,
+  sweep: boolean,
+  largeArc: boolean,
+) => {
   const d = `M ${start.x} ${start.y} A ${radius} ${radius} 0 ${
     largeArc ? 1 : 0
   } ${sweep ? 1 : 0} ${end.x} ${end.y}`;
 
   const center = getArc2Center(start, end, radius, sweep, largeArc);
-  console.log(center);
 
   const pathElement = document.createElementNS(
     'http://www.w3.org/2000/svg',
@@ -47,13 +48,12 @@ const drawArc2 = () => {
   pathElement.setAttributeNS(null, 'stroke', 'black');
   pathElement.setAttributeNS(null, 'fill', 'none');
 
-  const pointElement = document.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'circle',
-  );
-  pointElement.setAttributeNS(null, 'cx', '' + center.x);
-  pointElement.setAttributeNS(null, 'cy', '' + center.y);
-  pointElement.setAttributeNS(null, 'r', '5');
+  const centerPtElement = createSVGPoint(center);
+  const startPtElement = createSVGPoint(start);
+  const startTextElement = createSVGText(start, 'start');
+
+  const endPtElement = createSVGPoint(end);
+  const endTextElement = createSVGText(end, 'end');
 
   const svgElement = document.createElementNS(
     'http://www.w3.org/2000/svg',
@@ -62,8 +62,52 @@ const drawArc2 = () => {
   svgElement.setAttributeNS(null, 'width', '500');
   svgElement.setAttributeNS(null, 'height', '500');
   svgElement.appendChild(pathElement);
-  svgElement.appendChild(pointElement);
+  svgElement.appendChild(centerPtElement);
+  svgElement.appendChild(startPtElement);
+  svgElement.appendChild(startTextElement);
+  svgElement.appendChild(endPtElement);
+  svgElement.appendChild(endTextElement);
   document.body.appendChild(svgElement);
 };
 
-drawArc2();
+const createSVGPoint = (pt: Point) => {
+  const el = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  el.setAttributeNS(null, 'cx', '' + pt.x);
+  el.setAttributeNS(null, 'cy', '' + pt.y);
+  el.setAttributeNS(null, 'r', '5');
+  return el;
+};
+
+const createSVGText = (pt: Point, content: string) => {
+  const el = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+  el.setAttributeNS(null, 'x', '' + pt.x);
+  el.setAttributeNS(null, 'y', '' + (pt.y + 20));
+  el.innerHTML = content;
+  return el;
+};
+
+const start = { x: 100, y: 100 };
+const end = { x: 250, y: 200 };
+const radius = 95;
+const sweep = true;
+const largeArc = true;
+drawArc2(start, end, radius, sweep, largeArc);
+
+const drawArc3 = () => {
+  const start = { x: 100, y: 100 };
+  const end = { x: 250, y: 200 };
+  const bulge: number = -1.6;
+
+  if (bulge === 0) {
+    console.log('表达的是直线');
+    return;
+  }
+
+  const sweep = bulge > 0 ? false : true;
+  const largeArc = Math.abs(bulge) > 1 ? true : false;
+  const sweepAngle = Math.atan(Math.abs(bulge)) * 4;
+  const radius = distance(start, end) / 2 / Math.sin(sweepAngle / 2);
+  drawArc2(start, end, radius, sweep, largeArc);
+};
+
+drawArc3();
