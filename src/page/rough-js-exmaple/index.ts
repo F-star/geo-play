aa81f75bcf813231bfe7ba465497f0895de0f8de
xigua <hhbuhuiai@163.com>
2024-07-22 00:37:13 +0800

    feat: update rough.js

--- src/page/rough-js-exmaple/index.ts
+++ src/page/rough-js-exmaple/index.ts
@@ -1,11 +1,41 @@
-import { RoughCanvas } from 'roughjs/bin/canvas';
+// import { RoughCanvas } from 'roughjs/bin/canvas';
+import rough from 'roughjs';
 
-const roughCanvas = new RoughCanvas(document.querySelector('canvas')!);
+const canvas = document.querySelector('canvas')!;
+const ctx = canvas.getContext('2d')!;
 
-roughCanvas.rectangle(100, 100, 200, 200, { fill: 'red' });
+// ctx.lineWidth = 3;
+// ctx.strokeStyle = 'blue';
+// ctx.strokeRect(95, 95, 210, 210);
 
-const rect = roughCanvas.generator.rectangle(310, 310, 100, 100, {
-  fill: '#0f4',
+const roughCanvas = rough.canvas(canvas);
+
+const seed = rough.newSeed();
+
+console.log('seed', seed);
+
+roughCanvas.rectangle(80, 80, 200, 120, {
+  fill: 'red',
+  fillStyle: 'hachure',
+  fillWeight: 2,
+  disableMultiStroke: true,
+  hachureGap: 10,
+  // zigzagOffset: 10,
+  // strokeWidth: 2,
 });
 
-roughCanvas.draw(rect);
+// roughCanvas.circle(130, 130, 200, { fill: 'red' });
+
+// const circle = roughCanvas.generator.circle(130, 130, 200, {
+//   fill: 'red',
+//   roughness: 2,
+// });
+// console.log(circle);
+
+// roughCanvas.draw(circle);
+
+// const rect = roughCanvas.generator.rectangle(310, 310, 100, 100, {
+//   fill: '#0f4',
+// });
+// console.log(rect);
+// roughCanvas.draw(rect);
