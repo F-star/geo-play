cad99912a5778d38f2ba6cc9527b46b6ba188cba
huanghao <huanghao@current3d.com>
2024-07-31 16:00:01 +0800

    feat: 点到贝塞尔曲线的投影

---
+++ src/page/besizer-project/geo.ts
@@ -0,0 +1,92 @@
+import { distance } from '../../geo';
+import { Point } from '../../type';
+
+/** 计算三阶贝塞尔曲线 t 对应的点（套用参数方程公式） */
+export const getBezier3Point = (
+  p1: Point,
+  cp1: Point,
+  cp2: Point,
+  p2: Point,
+  t: number,
+) => {
+  const t2 = t * t;
+  const ct = 1 - t;
+  const ct2 = ct * ct;
+  const a = ct2 * ct;
+  const b = 3 * t * ct2;
+  const c = 3 * t2 * ct;
+  const d = t2 * t;
+
+  return {
+    x: a * p1.x + b * cp1.x + c * cp2.x + d * p2.x,
+    y: a * p1.y + b * cp1.y + c * cp2.y + d * p2.y,
+  };
+};
+
+/**
+ * 牛顿迭代法
+ * 当然贝塞尔曲线的参数方程并不单调，所以实际运算量还是很大，但能规避一些不必要的区域
+ * https://www.bilibili.com/video/BV1HQ4y1q78v/
+ *
+ * 一个 C 的算法库
+ * https://github.com/recp/cglm
+ */
+export const calcBezier3Project = (
+  p1: Point,
+  cp1: Point,
+  cp2: Point,
+  p2: Point,
+  targetPt: Point,
+  lookupTable: { t: number; pt: Point }[] = [],
+) => {
+  const count = 100;
+  if (lookupTable.length === 0) {
+    // 因为 t += step 这种写法会不断累加浮点数误差，所以换成 i / count 的写法
+    for (let i = 0; i <= count; i++) {
+      const t = i / count;
+      const pt = getBezier3Point(p1, cp1, cp2, p2, t);
+      lookupTable[i] = { t, pt };
+    }
+  }
+
+  let minDist = Number.MAX_SAFE_INTEGER;
+  let minIndex = -1;
+
+  for (let i = 0; i < lookupTable.length; i++) {
+    const item = lookupTable[i];
+    const dist = distance(targetPt, item.pt);
+    if (dist < minDist) {
+      minDist = dist;
+      minIndex = i;
+    }
+  }
+
+  let minT = lookupTable[minIndex].t;
+
+  const t1 = minIndex > 0 ? lookupTable[minIndex - 1].t : minT;
+  const t2 =
+    minIndex < lookupTable.length - 1 ? lookupTable[minIndex + 1].t : minT;
+
+  let step = 0.001;
+  for (let t = t1; t <= t2; t += step) {
+    const pt = getBezier3Point(p1, cp1, cp2, p2, t);
+    const dist = distance(targetPt, pt);
+    if (dist < minDist) {
+      minDist = dist;
+      minT = t;
+    }
+  }
+
+  if (minT < 0) {
+    minT = 0;
+  }
+  if (minT > 1) {
+    minT = 1;
+  }
+  const projectPt = getBezier3Point(p1, cp1, cp2, p2, minT);
+  return {
+    point: projectPt,
+    t: minT,
+    dist: distance(targetPt, projectPt),
+  };
+};
---
+++ src/page/besizer-project/index.ts
@@ -0,0 +1,73 @@
+import {
+  drawBezier,
+  drawLine,
+  fillPoints,
+  strokeCircle,
+} from '../../draw-util';
+import { Point } from '../../type';
+import { calcBezier3Project } from './geo';
+
+const canvas = document.querySelector('canvas')!;
+const ctx = canvas.getContext('2d')!;
+
+// https://stackoverflow.com/questions/4089443/find-the-tangent-of-a-point-on-a-cubic-bezier-curve
+
+const p1 = {
+  x: 100,
+  y: 206,
+};
+const p2 = {
+  x: 420,
+  y: 388,
+};
+const cp1 = {
+  x: 328,
+  y: 82,
+};
+const cp2 = {
+  x: 145,
+  y: 427,
+};
+
+// 光标点
+const targetPt = {
+  x: 400,
+  y: 100,
+};
+
+const lookupTable: { t: number; pt: Point }[] = [];
+
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  const projectInfo = calcBezier3Project(
+    p1,
+    cp1,
+    cp2,
+    p2,
+    targetPt,
+    lookupTable,
+  );
+
+  console.log('projectInfo', projectInfo);
+
+  ctx.strokeStyle = '#666';
+  drawBezier(ctx, p1, cp1, cp2, p2);
+
+  ctx.strokeStyle = '#f048';
+  strokeCircle(ctx, targetPt, projectInfo.dist);
+
+  drawLine(ctx, targetPt, projectInfo.point);
+
+  // 绘制点
+  fillPoints(ctx, [targetPt, projectInfo.point], 6);
+  ctx.restore();
+};
+
+draw();
+
+canvas.addEventListener('pointermove', (e: PointerEvent) => {
+  targetPt.x = e.clientX;
+  targetPt.y = e.clientY;
+  draw();
+});
