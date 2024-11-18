19d7f60c4c2491d4e758c48bfafb55df2f052b83
huanghao <hhbuhuiai@163.com>
2024-11-18 18:50:50 +0800

    feat: 透明度与图形

--- src/page/rect-with-opacity/index.ts
+++ src/page/rect-with-opacity/index.ts
@@ -1,3 +1,7 @@
+import InitCanvasKit from 'canvaskit-wasm/bin/canvaskit.js';
+// @ts-ignore
+import CanvasKitWasm from 'canvaskit-wasm/bin/canvaskit.wasm?url';
+
 const rect = {
   x: 100,
   y: 100,
@@ -6,8 +10,8 @@ const rect = {
 };
 const opacity = 0.5;
 
-const draw = () => {
-  const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const drawCanvas2d = () => {
+  const canvas = document.querySelector('#canvas2d') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d')!;
 
   ctx.fillStyle = '#00ff00';
@@ -37,4 +41,59 @@ const createTmpCanvas = () => {
   return canvas;
 };
 
-draw();
+const drawCanvasKit = async () => {
+  const CanvasKit = await InitCanvasKit({
+    locateFile: () => CanvasKitWasm,
+  });
+
+  const canvas = document.querySelector('#canvaskit') as HTMLCanvasElement;
+  const surface = CanvasKit.MakeWebGLCanvasSurface(canvas)!;
+
+  const paint = new CanvasKit.Paint();
+  paint.setColor(CanvasKit.Color4f(1, 0, 0, 1));
+  // paint.setAlphaf(opacity);
+
+  const paint2 = new CanvasKit.Paint();
+  paint2.setColor(CanvasKit.Color4f(0, 1, 0, 1));
+  paint2.setStyle(CanvasKit.PaintStyle.Stroke);
+  paint2.setStrokeWidth(20);
+  // paint2.setAlphaf(opacity);
+
+  /******* 图层使用的 ********/
+  const layerPaint = new CanvasKit.Paint();
+  paint2.setColor(CanvasKit.Color4f(0, 1, 0, 1));
+  layerPaint.setAlphaf(opacity);
+
+  const layerRect = CanvasKit.XYWHRect(50, 50, 300, 250);
+
+  const rect = CanvasKit.XYWHRect(100, 100, 200, 150);
+
+  surface.drawOnce((canvas) => {
+    canvas.clear(CanvasKit.WHITE);
+
+    canvas.scale(1.5, 1.5);
+
+    canvas.saveLayer(
+      layerPaint,
+      // layerRect,
+      // null,
+      // CanvasKit.SaveLayerF16ColorType,
+    );
+    // 设置透明度
+    canvas.drawRect(rect, paint);
+    canvas.drawRect(rect, paint2);
+
+    canvas.restore();
+
+    canvas.translate(0, 200);
+
+    paint.setAlphaf(opacity);
+    paint2.setAlphaf(opacity);
+
+    canvas.drawRect(rect, paint);
+    canvas.drawRect(rect, paint2);
+  });
+};
+
+drawCanvas2d();
+drawCanvasKit();
